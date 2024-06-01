ddab1cd5f3522a672173d402893dcf4d2fcac40c
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-06-01 16:17:23 +0900

    chore

--- index.js
+++ index.js
@@ -25,27 +25,16 @@ async function main() {
 
   const octokit = new Octokit({ auth: token });
   const { repository, after } = github.context.payload;
-  // const commit = await octokit.repos.getCommit({
-  //   owner: repository.owner.login,
-  //   repo: repository.name,
-  //   ref: after,
-  // });
 
   const compare = await octokit.repos.compareCommits({
     owner: repository.owner.login,
     repo: repository.name,
-    base: after + '^', // 最新のコミットの直前のコミット
+    base: after + '^',
     head: after,
   });
-  console.log('compare:', compare);
 
   const files = compare.data.files.map((file) => file.filename);
-  console.log(files);
-
-  //console.log('commit:', commit);
-  //const changedFiles = commit.data.files.map((file) => file.filename);
-  console.log('sha:', after);
-  //console.log(changedFiles);
+  console.log('files: ', files);
 }
 
 try {
@@ -53,16 +42,3 @@ try {
 } catch (error) {
   setFailed(error.message);
 }
-
-// try {
-//   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
-//   console.log(`Hello ${nameToGreet}!`);
-//   const time = (new Date()).toTimeString();
-//   core.setOutput("time", time);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
