e0135176446fb8bfa38e80552e31b34a6c8607fb
kengo-k <31839234+kengo-k@users.noreply.github.com>
2024-06-01 15:01:54 +0900

    add complex params

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+function main() {
+  console.log('Hello World');
+  const args = core.getInput('args');
+  console.log(args);
+}
+
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  main();
 } catch (error) {
   core.setFailed(error.message);
 }
+
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
